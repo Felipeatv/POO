1665e70c07596172ecb8f7d0829a60489ff5fb90
Felipe Tarapues <felipetarapues2106@gmail.com>
2024-09-10 15:42:07 -0500

    Agregue la calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,40 @@
-import java.util.Scanner;
-
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        Operaciones operaciones = new Operaciones();
-        Scanner entrada = new Scanner(System.in);
-        String opcion = "";
-        System.out.println("Bienvenido a la calculadora "+"/n");
-        System.out.println("1: Sumar, 2: Restar, 3: Multiplicar,4: Dividir, 5: Salir");
-        String opcion = entrada.nextLine();
-        while (!opcion.equals("5")) {
-            if (opcion.equals("1")) {
-                System.out.println("Ingrese el primer numero");
-                float num1 = entrada.nextFloat();
-                System.out.println("Ingrese el segundo numero");
-                float num2 = entrada.nextFloat();
-                System.out.println(operaciones.sumar(num1,num2));
-            }else if (opcion.equals("2")) {
-                System.out.println("Ingrese el primer numero");
-                float num1 = entrada.nextFloat();
-                System.out.println("Ingrese el segundo numero");
-                float num2 = entrada.nextFloat();
-                System.out.println(operaciones.restar(num1,num2));
-            }else if (opcion.equals("3")) {
-                System.out.println("Ingrese el primer numero");
-                float num1 = entrada.nextFloat();
-                System.out.println("Ingrese el segundo numero");
-                float num2 = entrada.nextFloat();
-                System.out.println(operaciones.multiplicar(num1,num2));
-            }else if (opcion.equals("4")) {
-                System.out.println("Ingrese el primer numero");
-                float num1 = entrada.nextFloat();
-                System.out.println("Ingrese el segundo numero");
-                float num2 = entrada.nextFloat();
-                System.out.println(operaciones.dividir(num1,num2));
-            }
+        float num1;
+        float num2;
+        int opcion;
+        float resultado;
+        JOptionPane.showMessageDialog(null, "Bienvenido a la Calculadora");
+        String Operaciones = JOptionPane.showInputDialog(null,"Ingrese la opcion:" + "\n" + "1.Sumar" + "\n" + "2.Restar" + "\n" + "3.Multiplicar" + "\n" + "4.Dividir");
+        switch (Operaciones) {
+            case "1":
+                num1 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el primer numero"));
+                num2 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el segundo numero"));
+                resultado = num1 + num2;
+                JOptionPane.showMessageDialog(null, resultado);
+                break;
+            case "2":
+                num1 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el primer numero"));
+                num2 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el segundo numero"));
+                resultado = num1 - num2;
+                JOptionPane.showMessageDialog(null, resultado);
+                break;
+            case "3":
+                num1 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el primer numero"));
+                num2 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el segundo numero"));
+                resultado = num1 * num2;
+                JOptionPane.showMessageDialog(null, resultado);
+                break;
+
+            case "4":
+                num1 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el primer numero"));
+                num2 = Float.parseFloat(JOptionPane.showInputDialog(null,"Ingrese el segundo numero"));
+                resultado = num1 / num2;
+                JOptionPane.showMessageDialog(null, resultado);
+                break;
         }
-
-
-
-
-
     }
 }
+
+
